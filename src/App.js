a8ecd6d9a5f1c6a8af2aaf35403c5f3290147c2d
AmbikaSubramanian <abisubu4.gmail.com>
2023-09-01 22:07:06 +0530

    files are added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App p-10">
+    <h4 className="text-6xl hover:bg-sky-700 text-yellow-300">Hello</h4>
+    <h1 className="text-palette-300">Hi im palette</h1>
+    <button className="border-2 py-1.5 border-red-500 md:border-blue-500 lg:border-green-500  rounded md:bg-red-500 lg:bg-green-500   md:border-blue-500 lg:border-green-500  text-neutral-950  hover:bg-transparent  hover:text-neutral-950 md:hover:text-red-500 lg:hover:text-black-500 " >sample</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      palette:
+{
+  50: '#ffe2e2',
+  100: '#ffb2b2',
+  200: '#ff8080',
+  300: '#fe4e4e',
+  400: '#fe1f1c',
+  500: '#e50a03',
+  600: '#b20301',
+  700: '#800000',
+  800: '#4e0000',
+  900: '#1f0000',
+}
+      }
+    },
+  },
+  plugins: [],
+}
